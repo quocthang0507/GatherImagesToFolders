084fb00bc5b1905a3d3e445a7c1f671dc4721700
La Quốc Thắng <quocthang_0507@yahoo.com.vn>
2020-09-04 06:19:59 +0700

    Add new functions

--- GatherImagesToFolders/Program.cs
+++ GatherImagesToFolders/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -13,16 +14,33 @@ namespace GatherImagesToFolders
 	{
 		private static string path_to_csv = @"D:\Github\GatherImagesToFolders\GatherImagesToFolders\data.csv";
 		private static string path_to_img_folder = @"D:\Github\102 Category Flower\jpg";
+		private static string path_to_img_folder2 = @"D:\102 Category Flower Dataset\jpg";
 		private static string path_to_categories_json = @"D:\Github\GatherImagesToFolders\GatherImagesToFolders\cat_to_name.json";
 		private static TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
 
 
 		static void Main(string[] args)
 		{
+			Function2();
+			Console.ReadKey();
+		}
+
+		/// <summary>
+		/// Gather images to folders
+		/// </summary>
+		static void Function1()
+		{
 			var data1 = ReadCsv();
 			var data2 = ReadJson();
 			GatherImages(data1, data2);
-			Console.ReadKey();
+		}
+
+		// Combine json and csv
+		static void Function2()
+		{
+			var data1 = ReadCsv();
+			var data2 = ReadJson();
+			WriteTagFile(data1, data2);
 		}
 
 		static List<KeyValuePair<int, int>> ReadCsv()
@@ -83,5 +101,32 @@ namespace GatherImagesToFolders
 				}
 			}
 		}
+
+		static void WriteTagFile(List<KeyValuePair<int, int>> listNumericLabel, List<KeyValuePair<int, string>> listTextLabel)
+		{
+			DirectoryInfo dir = new DirectoryInfo(path_to_img_folder2);
+			FileInfo[] files = dir.GetFiles();
+			using (StreamWriter writer = new StreamWriter(path_to_img_folder2 + "\\tag.csv"))
+			{
+				foreach (var item in files)
+				{
+					if (item.Extension == ".jpg")
+					{
+						// "img_00001.jpg" => "img_00001" => "00001"
+						string idStr = Path.GetFileNameWithoutExtension(item.FullName).Split('_')[1];
+						// "00001" => 1
+						int id = Convert.ToInt32(idStr);
+						int folderID = listNumericLabel.Where(pair => pair.Key == id).FirstOrDefault().Value;
+						var result = listTextLabel.Where(pair => pair.Key == folderID).FirstOrDefault().Value;
+						string label_output = textInfo.ToTitleCase(result);
+						string filename_input = item.Name;
+						string line = filename_input + '\t' + label_output;
+						writer.WriteLine(line);
+						Console.WriteLine(line);
+					}
+				}
+			}
+		}
+
 	}
 }
